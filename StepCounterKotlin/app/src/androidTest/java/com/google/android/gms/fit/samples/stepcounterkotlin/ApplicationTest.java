6feb66696bac300d15307a2fb3bd3bf9da2a4914
Garan Jenkin <garan@google.com>
2020-02-18 14:18:35 +0000

    Updated naming to match convention for Kotlin packages and directories.

--- StepCounterKotlin/app/src/androidTest/java/com/google/android/gms/fit/samples/stepcounterkotlin/ApplicationTest.java
+++ StepCounterKotlin/app/src/androidTest/java/com/google/android/gms/fit/samples/stepcounterkotlin/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.google.android.gms.fit.samples.stepcounter;
+package com.google.android.gms.fit.samples.stepcounterkotlin;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
